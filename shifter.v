810aa2e9ae2c3df45095098d232ccb317cc51237
Adrian Parvin Ouano <me@adrianpar.vin>
2023-10-10 05:31:33 +0800

    shifter: optimize

--- shifter.v
+++ shifter.v
@@ -12,9 +12,7 @@ module shifter #(
 integer i;
 
 wire [4:0] rb_ = rb;
-
 reg [31:0] ra_;
-reg [31:0] shift_out;
 
 always @* begin
   if (funct3[2])
@@ -24,14 +22,67 @@ always @* begin
         ra_[i] = ra[WIDTH-1-i];
 
   if (funct3[2])
-    out = shift_out;
+    out = shift16;
   else
     for(i=0;i<WIDTH;i=i+1)
-        out[i] = shift_out[WIDTH-1-i];
+        out[i] = shift16[WIDTH-1-i];
 end
 
-always @* begin
-  shift_out = {logic_alt & ra_[WIDTH-1], ra_} >>> rb_;
-end
+genvar j;
+
+wire [(WIDTH - 1):0] shift1;
+generate
+  for (j = 0; j < WIDTH - 1; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift1[j]), .I0(ra_[j]), .I1(ra_[j + 1]), .I2(rb[0]));
+  end
+
+  for (j = WIDTH - 1; j < WIDTH; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift1[j]), .I0(ra_[j]), .I1(logic_alt & ra_[WIDTH-1]), .I2(rb[0]));
+  end
+endgenerate
+
+wire [(WIDTH - 1):0] shift2;
+generate
+  for (j = 0; j < WIDTH - 2; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift2[j]), .I0(shift1[j]), .I1(shift1[j + 2]), .I2(rb[1]));
+  end
+
+  for (j = WIDTH - 2; j < WIDTH; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift2[j]), .I0(shift1[j]), .I1(logic_alt & ra_[WIDTH-1]), .I2(rb[1]));
+  end
+endgenerate
+
+wire [(WIDTH - 1):0] shift4;
+generate
+  for (j = 0; j < WIDTH - 4; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift4[j]), .I0(shift2[j]), .I1(shift2[j + 4]), .I2(rb[2]));
+  end
+
+  for (j = WIDTH - 4; j < WIDTH; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift4[j]), .I0(shift2[j]), .I1(logic_alt & ra_[WIDTH-1]), .I2(rb[2]));
+  end
+endgenerate
+
+wire [(WIDTH - 1):0] shift8;
+generate
+  for (j = 0; j < WIDTH - 8; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift8[j]), .I0(shift4[j]), .I1(shift4[j + 8]), .I2(rb[3]));
+  end
+
+  for (j = WIDTH - 8; j < WIDTH; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift8[j]), .I0(shift4[j]), .I1(logic_alt & ra_[WIDTH-1]), .I2(rb[3]));
+  end
+endgenerate
+
+wire [(WIDTH - 1):0] shift16;
+generate
+  for (j = 0; j < WIDTH - 16; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift16[j]), .I0(shift8[j]), .I1(shift8[j + 16]), .I2(rb[4]));
+  end
+
+  for (j = WIDTH - 16; j < WIDTH; j = j + 1) begin
+    LUT3 #(.INIT(8'b11001010)) A(.F(shift16[j]), .I0(shift8[j]), .I1(logic_alt & ra_[WIDTH-1]), .I2(rb[4]));
+  end
+endgenerate
 
 endmodule
