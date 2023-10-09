29251bc5f3b3f8b4234ac112ead2a383886d25ed
Adrian Parvin Ouano <me@adrianpar.vin>
2023-10-10 04:47:01 +0800

    uart: optimize

---
+++ mux8.v
@@ -0,0 +1,23 @@
+module _MUX8(input [7:0] x, input [2:0] sel, output y);
+
+wire a, b, c, d;
+wire ab, cd;
+
+(*keep*)
+LUT3 #(.INIT(8'b11001010)) A(.F(a), .I0(x[0]), .I1(x[1]), .I2(sel[0]));
+(*keep*)
+LUT3 #(.INIT(8'b11001010)) B(.F(b), .I0(x[2]), .I1(x[3]), .I2(sel[0]));
+(*keep*)
+LUT3 #(.INIT(8'b11001010)) C(.F(c), .I0(x[4]), .I1(x[5]), .I2(sel[0]));
+(*keep*)
+LUT3 #(.INIT(8'b11001010)) D(.F(d), .I0(x[6]), .I1(x[7]), .I2(sel[0]));
+
+(*keep*)
+MUX2_LUT5 AB(.I0(a), .I1(b), .S0(sel[1]), .O(ab));
+(*keep*)
+MUX2_LUT5 CD(.I0(c), .I1(d), .S0(sel[1]), .O(cd));
+
+(*keep*)
+MUX2_LUT6 Y(.I0(ab), .I1(cd), .S0(sel[2]), .O(y));
+
+endmodule
--- uart.v
+++ uart.v
@@ -16,9 +16,14 @@ module uart#(
     output rx_ack
 );
 
+localparam UART_IDLE = 0;
+localparam UART_START = 1;
+localparam UART_RUNNING = 2;
+localparam UART_STOP = 3;
+
 localparam HALF_CLOCKS_PER_BIT = CLOCKS_PER_BIT/2;
 
-reg [7:0] tx_shift_register = 0;
+reg [7:0] tx_shift_register;
 reg [7:0] tx_shift_registers [TX_FIFO-1:0];
 reg [$clog2(TX_FIFO):0] tx_read_i = 0;
 reg [$clog2(TX_FIFO):0] tx_write_i = 0;
@@ -26,8 +31,10 @@ reg [$clog2(TX_FIFO):0] tx_write_i = 0;
 reg [$clog2(CLOCKS_PER_BIT)-1:0] tx_divider = 0;
 wire [$clog2(CLOCKS_PER_BIT)-1:0] tx_next = tx_divider + 1;
 
-reg [3:0] tx_state = 14;
+reg [1:0] tx_state = UART_IDLE;
+reg [2:0] tx_index = 0;
 
+wire tx_empty = tx_read_i == tx_write_i;
 wire tx_full = (tx_read_i ^ tx_write_i) == TX_FIFO;
 wire tx_sample = tx_next == CLOCKS_PER_BIT;
 
@@ -40,29 +47,35 @@ always @(posedge clk) begin
   end
 end
 
+wire tx_muxed;
+_MUX8 TX_MUXED(.x(tx_shift_register), .sel(tx_index), .y(tx_muxed));
+
 always @(posedge clk) begin
-  if (tx_state == 14) begin
-    if (tx_read_i != tx_write_i) begin
-      tx_divider <= 0;
-      tx_state <= 15;
-      tx_shift_register <= tx_shift_registers[tx_read_i[$clog2(TX_FIFO):0]];
-      tx_read_i <= tx_read_i + 1;
-    end
-  end else begin
-    if (tx_sample) begin
-      if (tx_state == 15) begin
-        uart_tx <= 1'b0;
-      end else begin
-        uart_tx <= tx_shift_register[0];
-
-        tx_shift_register <= {1'b1, tx_shift_register[7:1]};
+  if (tx_sample) begin
+    case (tx_state)
+      UART_IDLE: begin
+        uart_tx <= 1;
+        if (!tx_empty) begin
+          tx_state <= UART_START;
+          tx_shift_register <= tx_shift_registers[tx_read_i[$clog2(TX_FIFO):0]];
+          tx_read_i <= tx_read_i + 1;
+        end
       end
+      UART_START: begin
+        uart_tx <= 0;
 
-      tx_state <= tx_state + 1;
-    end
+        tx_state <= UART_RUNNING;
+      end
+      UART_RUNNING: begin
+        uart_tx <= tx_muxed;
+        tx_index <= tx_index + 1;
 
-    tx_divider <= tx_sample ? 0 : tx_next;
+        if (tx_index == 7) tx_state <= UART_IDLE;
+      end
+    endcase
   end
+
+  tx_divider <= tx_sample ? 0 : tx_next;
 end
 
 reg [7:0] rx_shift_register = 0;
@@ -74,7 +87,8 @@ reg [$clog2(CLOCKS_PER_BIT)-1:0] rx_divider = 0;
 wire [$clog2(CLOCKS_PER_BIT)-1:0] rx_next = rx_divider + 1;
 wire rx_sample = rx_next == CLOCKS_PER_BIT;
 
-reg [3:0] rx_state = 9;
+reg [1:0] rx_state = UART_IDLE;
+reg [2:0] rx_index = 0;
 
 wire rx_empty = rx_read_i == rx_write_i;
 wire rx_full = (rx_read_i ^ rx_write_i) == RX_FIFO;
@@ -89,26 +103,42 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-  if (rx_state == 9) begin
+  if (rx_state == UART_IDLE) begin
     if (uart_rx == 0) begin
       rx_divider <= HALF_CLOCKS_PER_BIT;
-      rx_state <= 15;
+      rx_state <= UART_START;
     end
   end else begin
-    if (rx_divider == 0) begin
-      if (rx_state < 8) begin
-        rx_shift_register <= {uart_rx, rx_shift_register[7:1]};
-      end else if (rx_state == 8) begin
-        if (!rx_full) begin
-          rx_shift_registers[rx_write_i[$clog2(RX_FIFO)-1:0]] <= rx_shift_register;
-          rx_write_i <= rx_write_i + 1;
-        end
-      end
-
-      rx_state <= rx_state + 1;
+    if (rx_sample) begin
+      case (rx_state)
+        UART_START: begin
+          rx_state <= UART_RUNNING;
+        end      
+        UART_RUNNING: begin
+          if (rx_index == 7) rx_state <= UART_STOP;
+        end      
+        UART_STOP: begin
+          if (!rx_full) rx_shift_registers[rx_write_i[$clog2(RX_FIFO)-1:0]] <= rx_shift_register;
+          if (!rx_full) rx_write_i <= rx_write_i + 1;
+          rx_state <= UART_IDLE;
+        end      
+      endcase
     end
 
     rx_divider <= rx_sample ? 0 : rx_next;
+  end
+
+  if (rx_sample) begin
+    if (rx_index == 0) rx_shift_register[0] <= uart_rx;
+    if (rx_index == 1) rx_shift_register[1] <= uart_rx;
+    if (rx_index == 2) rx_shift_register[2] <= uart_rx;
+    if (rx_index == 3) rx_shift_register[3] <= uart_rx;
+    if (rx_index == 4) rx_shift_register[4] <= uart_rx;
+    if (rx_index == 5) rx_shift_register[5] <= uart_rx;
+    if (rx_index == 6) rx_shift_register[6] <= uart_rx;
+    if (rx_index == 7) rx_shift_register[7] <= uart_rx;
+
+    rx_index <= rx_state == UART_START ? 0 : rx_index + 1;
   end
 end
 
